be2032e0dbec614b932941c74010ef04357e88c7
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 22:29:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(customer_list)
     placement = ""
     i = 0
     while i < customer_list.length  
-      placement +="#{i + 1}.#{customer_list[i]}"
+      placement += "#{i + 1}.#{customer_list[i]}"
       i += 1
     end
     puts "The line is currently:#{placement}"
