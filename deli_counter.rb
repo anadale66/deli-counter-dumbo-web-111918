eeae8a360b3b0d4d737f756ada18b4eb6fe1b227
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 22:15:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,21 @@ def take_a_number(customer_list, name)
 end
 
 def line(customer_list)
-  if customer_list == []
-    
-  
+  if customer_list.length == 0
+    puts "The line is currently empty."
+  else
+    placement = []
+    i = 0 
+    while i < customer_list.length  
+    placement << "The line is currently: #{i + 1}. #{customer_list[i]}"
+    puts placement 
   end
+  
+  
+def now_serving(customer_list)
+  if cutomer_list.length == 0 
+    
+end
+  
   
 end
