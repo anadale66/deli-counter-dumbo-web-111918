03925ae39b9b2e33b79c045318c89b75d1cb304f
Roman Pirag <mr.anadale@yahoo.com>
2018-10-24 22:05:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+
+def take_a_number(customer_list, name)
+  customer_list << name 
+  puts "Welcome, #{name}. You are number #{customer_list.length} in line."
+end
+
+def line(customer_list)
+  if customer_list == []
+    
+  
+  end
+  
+end
